04b4eb5fdba79870da696ed6f1b43c1d79580584
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-10-09 22:04:33 -0700

    Cleanup code and UI

--- wav2bonk.py
+++ wav2bonk.py
@@ -8,7 +8,7 @@ from tkinter.filedialog import askopenfile
 from tkinter.ttk import *
 root = Tk()
 window_height = 600
-window_width = 400
+window_width = 250
 root.geometry(f'{window_height}x{window_width}')
 root.configure(background='#aaaaaa')
 root.title('Greenface Labs Wav2Bonk')
@@ -18,14 +18,13 @@ input_filename = 'none'
 def fileDialog():
   infile = askopenfile(mode='r', filetypes=[("Wav files", "*.wav")], initialdir='./wavfile')
   input_filename = infile.name
-  print(input_filename)
+  # print(input_filename)
   parts = input_filename.split(".")
   dirs = parts[0].split("/")
   output_filename = "output/" + dirs[len(dirs)-1]+".txt"
-  print(output_filename)
 
   samrate, data = wavfile.read(str(input_filename))
-  print(data)
+  # print(data)
 
   wavData = pd.DataFrame(data)
 
@@ -34,18 +33,14 @@ def fileDialog():
   index = pd.date_range('1/1/2000', periods=input_len, freq='T')
 
   if len(wavData.columns) == 2:
-      print('Stereo .wav file not supported!\n')
-      sys.exit()
+      display_text.set('Stereo .wav file not supported!')
 
   elif len(wavData.columns) == 1:
-      #print('Loaded mono .wav file: '+input_filename)
-      #print('\n')
       f = open(output_filename, "w")
       f.write("w0,")
       
       wavData.index = index
       sf = input_len / 128
-      # print(sf)
       wavData = (wavData.resample(str(sf)+'T').mean())
       # print(index)
       # print(wavData)
@@ -62,43 +57,23 @@ def fileDialog():
         i = i + 1
 
       f.close()
-      #print('Save is done to: ' + output_filename + " Length: " + str(i))
-      display_text.set('Saved: ' + output_filename + " Length: " + str(i))
-      #the_label.cget("text") = 'Save is done to: ' + output_filename + " Length: " + str(i)
+      display_text.set('Saved: ' + output_filename)
 
   else:
-      print('Multi channel .wav file not supported!\n')
-      sys.exit()
+      display_text.set('Multi channel .wav file not supported!')
 
-
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-x_cordinate = int((screen_width/2) - (window_width/2))
-y_cordinate = int((screen_height/2) - (window_height/2))
-
-style = ttk.Style()
-style.configure("BW.Frm", foreground="red", background="white")
 
 s = ttk.Style()
-s.configure('Danger.TFrame', background='#9999ff', borderwidth=5, anchor="center", relief='sunken')
+s.configure('Danger.TFrame', background='#99aaff', borderwidth=5, anchor="center", relief='sunken')
 
 frm = ttk.Frame(root, padding=10, style='Danger.TFrame')
 frm.pack(pady=20)
 
 display_text = tk.StringVar()
-display_text.set("Greenface Labs .wav to Bonkulator.")
+display_text.set("Greenface Labs: .wav to Bonkulator")
 the_label = ttk.Label(frm, textvariable=display_text, font="verdana 16", background="#444444", foreground="white").pack()
 
 the_button = ttk.Button(frm, text="Select File", command=fileDialog).pack(pady=10)
 exit_button = ttk.Button(frm, text="Done", command=root.destroy).pack(pady=10)
-
-#the_button.text = "Done"
-#the_button.command = root.destroy
-
-#infile = askopenfile(mode='r', filetypes=[("Wav files", "*.wav")], initialdir='./wavfile')
-#input_filename = infile.name
-#root.mainloop()
-
 
 root.mainloop()
