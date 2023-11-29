d1e182a58f04f763cf276846fc7d19f6a0e2b453
Shannon Vance <shannon@greenlightdevelopment.com>
2023-11-29 15:00:53 -0800

    Oops, we need _internal

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pyinstaller --onedir --hidden-import=pandas --hidden-import=scipy --add-data "wavfile:wavfile"  wav2bonk.py
-zip -r wav2bonk_mac.zip dist/wav2bonk -x "*_internal*"
+zip -r wav2bonk_mac.zip dist/wav2bonk
