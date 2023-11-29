dce1fe134a629976220bd1fd2739540c14ca27f9
Shannon Vance <shannon@greenlightdevelopment.com>
2023-11-29 14:49:00 -0800

    Forgot -r in zip command

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pyinstaller --onedir --hidden-import=pandas --hidden-import=scipy --add-data "wavfile:wavfile"  wav2bonk.py
-zip wav2bonk.zip dist/wav2bonk
+zip -r wav2bonk.zip dist/wav2bonk
